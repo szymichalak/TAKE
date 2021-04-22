ff2a412ef95b776692be9051d50aed3137e85289
szymichalak <szymonmichalak97@gmail.com>
2021-04-22 13:53:57 +0200

    add exercise 5

---
+++ zad5/MDBLab/MDBLab-ejb/build/generated-sources/ap-source-output/ejb/NewsItem_.java
@@ -0,0 +1,15 @@
+package ejb;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-22T13:52:15")
+@StaticMetamodel(NewsItem.class)
+public class NewsItem_ { 
+
+    public static volatile SingularAttribute<NewsItem, String> heading;
+    public static volatile SingularAttribute<NewsItem, Long> id;
+    public static volatile SingularAttribute<NewsItem, String> body;
+
+}
