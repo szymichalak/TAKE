25b01c7641bd651c987ee276029fa232baac265f
szymichalak <szymonmichalak97@gmail.com>
2021-05-13 14:58:44 +0200

    add exercise 9

---
+++ zad9/CompanyApi/CompanyApi/Models/EmployeeDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyApi.Models
+{
+    public class EmployeeDTO
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? ManagerId { get; set; }
+        public decimal? Salary { get; set; }
+        public decimal? Bonus { get; set; }
+        public int? DepartmentId { get; set; }
+
+
+        private static EmployeeDTO EmployeeToDTO(Employee employee) =>
+         new EmployeeDTO
+         {
+             EmployeeId = employee.EmployeeId,
+             FirstName = employee.FirstName,
+             LastName = employee.LastName,
+             ManagerId = employee.ManagerId,
+             Salary = employee.Salary,
+             Bonus = employee.Bonus,
+             DepartmentId = employee.DepartmentId
+         };
+        private static Employee DTOToEmployee(EmployeeDTO employeeDTO) =>
+        new Employee
+        {
+            EmployeeId = employeeDTO.EmployeeId,
+            FirstName = employeeDTO.FirstName,
+            LastName = employeeDTO.LastName,
+            ManagerId = employeeDTO.ManagerId,
+            Salary = employeeDTO.Salary,
+            Bonus = employeeDTO.Bonus,
+            DepartmentId = employeeDTO.DepartmentId
+        };
+    }
+}
